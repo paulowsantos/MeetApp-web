a0fb91a4a535175defde4a30cfec86745e1550d1
Paulo Wayner <paulowsantos@gmail.com>
2019-10-14 02:11:21 -0700

    add auth styles

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '../../assets/M.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Sign In</button>
+        <Link to="/register">Sign up for free</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '../../assets/M.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <form>
+        <input placeholder="Full name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Sign Up</button>
+        <Link to="/">Already a member</Link>
+      </form>
+    </>
+  );
 }
