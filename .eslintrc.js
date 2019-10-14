ae3724ad5b7121bb3fcafdbf75775d431442d3f7
Paulo Wayner <paulowsantos@gmail.com>
2019-10-14 01:39:57 -0700

    removed config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,11 +48,4 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
-  settings: {
-    'import/resolver': {
-      'babel-plugin-root-import': {
-        rootPathSuffix: 'src'
-      },
-    },
-  },
 };
