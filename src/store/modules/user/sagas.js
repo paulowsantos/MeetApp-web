7e5788f4e400a723cb1fa7d65a548ef0d4bf58f9
Paulo Wayner <paulowsantos@gmail.com>
2019-10-17 01:04:39 -0700

    added profile update and avatar func

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,31 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '../../../services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.password ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile updated.');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Error updating profile. Check the data.');
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
